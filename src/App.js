3cc0aab57001b4fdd049df92f909811714879c63
MdBasharAfzal <basharafzal.t.n@gmail.com>
2023-06-10 16:07:57 +0530

    Responsive

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./Components/header/Header";
+import Calendar from "./Components/calendar/Calendar";
+
+
+function App() {
+  return (
+    <div >
+      <Header></Header>
+      <Calendar></Calendar>
+    </div>
+  );
+}
+
+export default App;
